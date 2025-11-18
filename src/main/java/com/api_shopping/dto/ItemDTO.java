0df4acb63fa2b19b2ec3baa0770e4258f5434688
willian-alves <willian.alves@turingbits.com.br>
2025-11-18 00:11:46 -0300

    feat: Classe ItemDTO Criada

---
+++ src/main/java/com/api_shopping/dto/ItemDTO.java
@@ -0,0 +1,11 @@
+package com.api_shopping.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDTO {
+    private String productIdentifier;
+    private Float price;
+}
