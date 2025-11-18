8d325093c7cdb3f9d4f3e1314f20ef3b72c1a195
willian-alves <willian.alves@turingbits.com.br>
2025-11-18 00:34:26 -0300

    feat: Criado metodo conversor de Item para ItemDTO

--- src/main/java/com/api_shopping/dto/ItemDTO.java
+++ src/main/java/com/api_shopping/dto/ItemDTO.java
@@ -1,5 +1,6 @@
 package com.api_shopping.dto;
 
+import com.api_shopping.entities.Item;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,4 +9,11 @@ import lombok.Setter;
 public class ItemDTO {
     private String productIdentifier;
     private Float price;
+
+    public static ItemDTO convertToItemDTO(Item item) {
+        ItemDTO itemDTO = new ItemDTO();
+        itemDTO.setProductIdentifier(item.getProductIdentifier());
+        itemDTO.setPrice(item.getPrice());
+        return itemDTO;
+    }
 }
