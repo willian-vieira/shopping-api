b71086f2f3e08feb160727dd27cc0458e24f8f05
willian-alves <willian.alves@turingbits.com.br>
2025-11-18 00:31:23 -0300

    feat: Criado metodo conversor de Shop para ShopDTO

--- src/main/java/com/api_shopping/dto/ShopDTO.java
+++ src/main/java/com/api_shopping/dto/ShopDTO.java
@@ -1,8 +1,10 @@
 package com.api_shopping.dto;
 
+import com.api_shopping.entities.Shop;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,4 +12,14 @@ public class ShopDTO {
     private String userIdentifier;
     private float total;
     private LocalDateTime date;
+
+    private List<ItemDTO> itemDTOS;
+
+    public static ShopDTO convertToShopDTO(Shop shop) {
+        ShopDTO shopDTO = new ShopDTO();
+        shopDTO.setUserIdentifier(shop.getUserIdentifier());
+        shopDTO.setTotal(shop.getTotal());
+        shopDTO.setDate(shop.getDate());
+        return shopDTO;
+    }
 }
