f4ad21d23cc43757e8de77fe35d3ad4d73cc0211
willian-alves <willian.alves@turingbits.com.br>
2026-01-21 23:42:02 -0300

    feat: Criados novos Controllers getShopsFilter e getReportByDate

--- src/main/java/com/api_shopping/controller/ShopController.java
+++ src/main/java/com/api_shopping/controller/ShopController.java
@@ -1,10 +1,13 @@
 package com.api_shopping.controller;
 
 import com.api_shopping.dto.ShopDTO;
+import com.api_shopping.dto.ShopReportDTO;
 import com.api_shopping.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -39,5 +42,27 @@ public class ShopController {
     @PostMapping("/shoppping")
     public ShopDTO newShop(@Validated @RequestBody ShopDTO shopDTO) {
         return this.shopService.save(shopDTO);
+    }
+
+    @GetMapping("shopping/search")
+    public List<ShopDTO> getShopsFilter(
+        @RequestParam(name = "dataInicio", required = true)
+        @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataInicio,
+        @RequestParam(name = "dataFim", required = false)
+        @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataFim,
+        @RequestParam(name = "valorMinimo", required = false)
+        Float valorMinimo
+    ) {
+        return shopService.getShopByFiler(dataInicio, dataFim, valorMinimo);
+    }
+
+    @GetMapping("shopping/report")
+    public ShopReportDTO getReportByDate(
+        @RequestParam(name = "dataInicio", required = true)
+        @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataInicio,
+        @RequestParam(name = "dataFim", required = true)
+        @DateTimeFormat(pattern = "dd/MM/yyyy") Date dataFim
+    ) {
+        return shopService.getReportByDate(dataInicio, dataFim);
     }
 }
