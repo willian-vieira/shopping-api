ec86550e77488b00f6df9de7d549a09f04017d5b
willian-alves <willian.alves@turingbits.com.br>
2026-01-21 23:39:34 -0300

    feat: Implementação da Interface IReportRepository

--- src/main/java/com/api_shopping/repository/ReportRepositoryImpl.java
+++ src/main/java/com/api_shopping/repository/ReportRepositoryImpl.java
@@ -5,6 +5,7 @@ import com.api_shopping.entities.Shop;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class ReportRepositoryImpl implements IReportRepository {
     public List<Shop> getShopByFilters(Date dataInicio, Date dataFim, Float valorMinimo) {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT s ");
-        sql.append("FROM shop s ");
+        sql.append("FROM Shop s ");
         sql.append("WHERE s.date >= :dataInicio ");
 
         if (dataFim != null) {
@@ -27,7 +28,7 @@ public class ReportRepositoryImpl implements IReportRepository {
             sql.append("AND s.total <= :valorMinimo ");
         }
 
-        Query query = entityManager.createQuery(sql.toString());
+        Query query = entityManager.createQuery(sql.toString(), Shop.class);
         query.setParameter("dataInicio", dataInicio);
 
         if (dataFim != null) {
@@ -42,6 +43,21 @@ public class ReportRepositoryImpl implements IReportRepository {
 
     @Override
     public ShopReportDTO getReportByDate(Date dataInicio, Date dataFim) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT COUNT(sp.id), sum(sp.total), avg(sp.total) ");
+        sql.append("FROM Shop sp ");
+        sql.append("WHERE sp.date >= :dataInicio ");
+        sql.append("AND sp.date <= :dataFim ");
+
+        Query query = entityManager.createNativeQuery(sql.toString(), ShopReportDTO.class);
+        query.setParameter("dataInicio", dataInicio);
+        query.setParameter("dataFim", dataFim);
+
+        Object[] result = (Object[]) query.getSingleResult();
+        ShopReportDTO shopReportDTO = new ShopReportDTO();
+        shopReportDTO.setCount(((BigDecimal) result[0]).intValue());
+        shopReportDTO.setTotal((Double) result[1]);
+        shopReportDTO.setMean((Double) result[1]);
         return null;
     }
 }
